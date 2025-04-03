1a32dc387c1140a50b65e0331bcc67b8466f6a90
gemaderus <hi@gemaderus.com>
2025-04-03 05:43:39 +0200

    feat: add configuration for templates

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const path = require("path");
+const expresHbs = require("express-handlebars");
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const bodyParser = require("body-parser");
 
 const app = express();
 
+app.engine(
+  "hbs",
+  expresHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page not found" });
 });
 
 app.listen(8080);
