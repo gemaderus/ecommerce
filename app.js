0bdb7f64c2217fdefd1dc4d458fd0206330750e8
gemaderus <hi@gemaderus.com>
2025-04-04 12:35:35 +0200

    feat: refactor controllers

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const expresHbs = require("express-handlebars");
+const productsController = require("./controllers/error");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 const bodyParser = require("body-parser");
@@ -17,17 +18,17 @@ app.engine(
     extname: "hbs",
   })
 );
-app.set("view engine", "hbs");
+// app.set("view engine", "hbs");
+
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page not found" });
-});
+app.use(productsController.get404);
 
 app.listen(8080);
--- routes/shop.js
+++ routes/shop.js
@@ -3,22 +3,11 @@ const rootDir = require("../util/path");
 const adminData = require("./admin");
 
 const express = require("express");
-console.log("rootDir", rootDir);
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
+const productsController = require("../controllers/products");
 
-  console.log("products", products);
-  res.render("shop", {
-    prods: products,
-    pageTitle: "Shop",
-    path: "/",
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true,
-  });
-});
+router.get("/", productsController.getProducts);
 
 module.exports = router;
