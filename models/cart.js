28c2dae6e3e3fb7fd916f7b65b604be8f8e6dc95
gemaderus <hi@gemaderus.com>
2025-04-29 12:01:08 +0200

    feat: add delete logic.

--- models/cart.js
+++ models/cart.js
@@ -43,8 +43,14 @@ module.exports = class Cart {
       if (err) {
         return;
       }
+
       const updatedCart = { ...JSON.parse(fileContent) };
       const product = updatedCart.products.find((prod) => prod.id === id);
+
+      if (!product) {
+        return;
+      }
+
       const productQty = product.qty;
       updatedCart.products = updatedCart.products.filter(
         (prod) => prod.id !== id
