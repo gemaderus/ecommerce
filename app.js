93831aa3e8e66b5a3ef1ebd01d58e7eeb1ab008b
gemaderus <hi@gemaderus.com>
2025-05-19 05:47:00 +0200

    feat: add user in request

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const expresHbs = require("express-handlebars");
 const productsController = require("./controllers/error");
-const mongoConnect = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
-// const User = require("./models/user");
+const User = require("./models/user");
 
-// const adminRoutes = require("./routes/admin");
-// const shopRoutes = require("./routes/shop");
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 const bodyParser = require("body-parser");
 
@@ -30,29 +30,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then((user) => {
-  //     if (!user) {
-  //       throw new Error("User not found");
-  //     }
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch((err) => {
-  //     console.error("Error fetching user:", err);
-  //     next(err); // Pass the error to the error-handling middleware
-  //   });
+  User.findById("68270fef4d4bc0a8670e4cef")
+    .then((user) => {
+      if (!user) {
+        throw new Error("User not found");
+      }
+
+      req.user = user;
+
+      next();
+    })
+    .catch((err) => {
+      console.error("Error fetching user:", err);
+    });
 });
 
-// app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes);
 
-// app.use(shopRoutes);
+app.use(shopRoutes);
 
 app.use(productsController.get404);
 
-mongoConnect((client) => {
-  console.log("client", client);
-  app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+mongoConnect(() => {
+  app.listen(8080, () => {
+    console.log("Server is running on port 8080");
   });
 });
