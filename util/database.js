82949c05693b605fd8397e6da54b42a20a41793a
gemaderus <hi@gemaderus.com>
2025-05-19 05:48:48 +0200

    feat: implement mongodb in database

--- util/database.js
+++ util/database.js
@@ -1,18 +1,29 @@
 const mongodb = require("mongodb");
-
 const MongoClient = mongodb.MongoClient;
+
+let db;
 
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    "mongodb+srv://gemaderus:Y5vyTY4z00xEnahy@cluster0.6qsl0eu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    "mongodb+srv://gemaderus:Y5vyTY4z00xEnahy@cluster0.6qsl0eu.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
   )
     .then((client) => {
       console.log("Connected");
-      callback(client);
+      db = client.db();
+      callback();
     })
     .catch((err) => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (db) {
+    return db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
